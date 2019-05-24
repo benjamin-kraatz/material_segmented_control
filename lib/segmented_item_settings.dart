8f7149c3f7ed2ce13f2b07061731bdf2544e5382
BENN <benjaminkraatz@yahoo.com>
2019-05-24 22:54:36 +0200

    Docs for SegmentedItemSettings class

--- lib/segmented_item_settings.dart
+++ lib/segmented_item_settings.dart
@@ -1,29 +1,41 @@
 import 'segmented_children.dart';
 
+/// Holds information to set up a [SegmentedItem]
 class SegmentedItemSettings {
   double _borderRadius;
   bool _isSelectable;
 
+  /// Define a border radius with [_borderRadius]
+  /// And if an item can be selected through [_isSelectable]
   SegmentedItemSettings(this._borderRadius, this._isSelectable);
 
+  /// Get the current [_borderRadius] setting
   double get borderRadius => _borderRadius;
+
+  /// Get the current [_isSelectable] setting
   bool get isSelectable => _isSelectable;
 
+  /// Sets the border radius to an instance of [SegmentedItemSettings]
+  /// and returns it with set value
   SegmentedItemSettings setBorderRadius(double radius) {
     _borderRadius = radius;
     return this;
   }
 
+  /// Sets the selection state to an instance of [SegmentedItemSettings]
+  /// and returns it with set value
   SegmentedItemSettings setReselectable(bool state) {
     _isSelectable = state;
     return this;
   }
 
+  /// Singleton creator
   static SegmentedItemSettings make() {
     return SegmentedItemSettings(0, false);
   }
 }
 
+/// Used to define the current selected item
 class SegmentedItemSelectorWrapper {
   final SegmentedItem itemSelected;
   final bool isSelected;
