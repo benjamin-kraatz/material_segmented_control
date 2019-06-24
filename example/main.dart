789b7700f7b44e1dc3de1b390ca4ab6868a4256b
BENN <benjaminkraatz@yahoo.com>
2019-06-24 23:14:11 +0200

    Provided the Cupertino original code, then trying to modify it for material

--- example/main.dart
+++ example/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         child: MaterialSegmentedControl(
           borderRadius: 32.0,
           initialSelection: 0,
+          onSelected: (val) {},
           children: <SegmentedItem>[
             SegmentedItem(
               colorSelected: Colors.lightBlueAccent,
