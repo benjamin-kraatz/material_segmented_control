37b03b6d056907b56d5c2c5a4468035eba483036
Vinícius Salmont <viniciussalmont@icloud.com>
2019-10-22 17:01:51 -0300

    Added param vertical offset

--- lib/material_segmented_control.dart
+++ lib/material_segmented_control.dart
@@ -45,6 +45,7 @@ class MaterialSegmentedControl<T> extends StatefulWidget {
       this.selectedColor,
       this.borderColor,
       this.borderRadius = 32.0,
+      this.verticalOffset = 12.0,
       this.horizontalPadding = _horizontalPadding,})
       : assert(children != null),
         assert(children.length >= 2),
@@ -101,6 +102,11 @@ class MaterialSegmentedControl<T> extends StatefulWidget {
   ///
   /// Defaults to 16 if null
   final EdgeInsets horizontalPadding;
+  
+  // The vertical offset is used on top and bottom distance
+  ///
+  /// Defaults to 12 if null
+  final double verticalOffset;
 
   @override
   _SegmentedControlState<T> createState() => _SegmentedControlState<T>();
@@ -124,8 +130,6 @@ class _SegmentedControlState<T> extends State<MaterialSegmentedControl<T>>
   Color _unselectedTextColor;
   Color _borderColor;
   Color _pressedColor;
-
-  final double _verticalOffset = 12.0;
 
   AnimationController createAnimationController() {
     return AnimationController(
@@ -299,7 +303,7 @@ class _SegmentedControlState<T> extends State<MaterialSegmentedControl<T>>
 
       Widget child = Center(
         child: Padding(
-            padding: EdgeInsets.symmetric(vertical: _verticalOffset),
+            padding: EdgeInsets.symmetric(vertical: widget.verticalOffset),
             child: widget.children[currentKey]),
       );
 
